76403d3b7f032b44a96ab8ada0104a63244123f1
HAN YANG <hyhx_2008@163.com>
2018-01-12 12:07:27 +0800

    add log4cpp

--- Makefile
+++ Makefile
@@ -7,12 +7,12 @@ MOD_NAME=we_earn
 
 APP_EXE=./$(MOD_NAME)
 
-APP_LIB	= ./lib/trader_api_636/thostmduserapi.so ./lib/trader_api_636/thosttraderapi.so
+APP_LIB	= ./lib/trader_api_636/thostmduserapi.so ./lib/trader_api_636/thosttraderapi.so ./lib/log4cpp/liblog4cpp.a -lpthread
 
 SUB_MODULES= . md_spi trade_spi account login_account strategy
 
 CINC += $(foreach dir,$(SUB_MODULES),-I $(dir))
-CINC += -I ./lib/trader_api_636
+CINC += -I ./lib/trader_api_636 -I ./include/log4cpp
 
 ALL_CFILES = $(foreach dir, $(SUB_MODULES), $(wildcard $(dir)/*.c))
 
--- main.cpp
+++ main.cpp
@@ -5,6 +5,11 @@
 #include "error_def.h"
 #include "md_spi.h"
 #include "trade_spi.h"
+#include "md_mgr.h"
+
+#include <log4cpp/Category.hh>
+#include <log4cpp/PropertyConfigurator.hh>
+
 
 const char *gs_md_front_addr[] = {"tcp://180.168.146.187:10013" //16-9
                           , "tcp://180.168.146.187:10011" //real time
@@ -20,9 +25,38 @@ const char *gs_td_front_addr[] = {"tcp://180.168.146.187:10030"
 
 TD_SPI *td_spi;
 
+
+
+//
+// internal functions declare
+//
+
+static int log4cpp_init();
+
+//
+// main
+//
+
 int main()
 {
-	int ret = 0;
+    int ret = 0;
+
+
+    ret = log4cpp_init();
+    if (ret != 0)
+    {
+        printf("log4cpp init error!\n");
+        return ret;
+    }
+
+    ret = md_mgr_init();
+    if (ret != 0)
+    {
+        return ret;
+    }
+
+
+	
 	// set true to use udp
 	CThostFtdcMdApi *md_api = CThostFtdcMdApi::CreateFtdcMdApi("./output", false);
 	if (!md_api) 
@@ -56,4 +90,42 @@ int main()
 
 	return 0;
 }
+
+//
+// internal functions
+//
+
+int log4cpp_init()
+{
+    std::string initFileName = "cfg/log4cpp.properties";
+    log4cpp::PropertyConfigurator::configure(initFileName);
+
+    log4cpp::Category& root = log4cpp::Category::getRoot();
+
+    log4cpp::Category& sub1 = 
+    	log4cpp::Category::getInstance(std::string("sub1"));
+
+    log4cpp::Category& sub2 = 
+    	log4cpp::Category::getInstance(std::string("sub1.sub2"));
+
+    root.warn("Storm is coming");
+
+    sub1.debug("Received storm warning");
+    sub1.info("Closing all hatches");
+
+    sub2.debug("Hiding solar panels");
+    sub2.error("Solar panels are blocked");
+    sub2.debug("Applying protective shield");
+    sub2.warn("Unfolding protective shield");
+    sub2.info("Solar panels are shielded");
+
+    sub1.info("All hatches closed");
+
+    root.info("Ready for storm.");
+
+    log4cpp::Category::shutdown();
+
+    return 0;
+}
+
 
